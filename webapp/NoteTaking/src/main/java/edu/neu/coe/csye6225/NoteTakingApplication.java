21b0c8c762a422d3ac9d619cc94f8398835be9d2
junyi <fang.ju@husky.neu.edu>
2019-01-29 14:55:13 -0500

    application pro

--- webapp/NoteTaking/src/main/java/edu/neu/coe/csye6225/NoteTakingApplication.java
+++ webapp/NoteTaking/src/main/java/edu/neu/coe/csye6225/NoteTakingApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NoteTakingApplication {
+class NoteTakingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NoteTakingApplication.class, args);
