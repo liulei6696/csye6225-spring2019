1e96a9ab3d7d873a5fa28d5a054042517c1e9534
zhuziyan96 <zhu.ziy@husky.neu.edu>
2019-01-29 14:31:56 -0500

    add some annotations and implements serializable

--- webapp/NoteTaking/src/main/java/edu/neu/coe/csye6225/mapper/UserMapper.java
+++ webapp/NoteTaking/src/main/java/edu/neu/coe/csye6225/mapper/UserMapper.java
@@ -1,9 +1,11 @@
 package edu.neu.coe.csye6225.mapper;
 
 import edu.neu.coe.csye6225.entity.User;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserMapper {
     List<User> getAllUsers();
 
