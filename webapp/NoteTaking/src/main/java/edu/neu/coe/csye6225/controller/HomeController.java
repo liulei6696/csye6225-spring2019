7ce4f98089cd0ed893867b308c60fd7eb8642f7d
junyi <fang.ju@husky.neu.edu>
2019-01-30 18:37:32 -0500

    bug fix

--- webapp/NoteTaking/src/main/java/edu/neu/coe/csye6225/controller/HomeController.java
+++ webapp/NoteTaking/src/main/java/edu/neu/coe/csye6225/controller/HomeController.java
@@ -21,10 +21,10 @@ public class HomeController {
 //        response.add("world");
 //        return response;
 //    }
-@RequestMapping(value = "/")
-    public String getCurrentTime() {
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
-        return df.format(new Date());// new Date()为获取当前系统时间
-    }
+//@RequestMapping(value = "/")
+//    public String getCurrentTime() {
+//        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+//        return df.format(new Date());// new Date()为获取当前系统时间
+//    }
 
 }
