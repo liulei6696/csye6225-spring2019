8cb73e35c3c9213e9b677b849b7bdd1ed9b3848a
zhuziyan96 <zhu.ziy@husky.neu.edu>
2019-03-12 16:32:29 -0400

    correct mistakes

--- infrastructure/aws/cloudformation/csye6225-aws-cd-create-policies-roles-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cd-create-policies-roles-stack.sh
@@ -1,9 +1,15 @@
 #! /bin/bash
 set -e
-echo "Input the stack name which you want to create"
-read name
-aws cloudformation create-stack --stack-name $name --template-body file://csye6225-cf-networking.yaml
-echo "Processing, please wait"
-aws cloudformation wait stack-create-complete --stack-name $name
+read -p "Input name of the stack: " stackName
+read -p "Input name of the bucket: " bucketName
+aws cloudformation create-stack --stack-name $stackName --template-body file://csye6225-cf-policies_roles-application.yaml --capabilities "CAPABILITY_NAMED_IAM" --parameters  "ParameterKey=bucket,ParameterValue=$bucketName"
+echo "creating"
+aws cloudformation wait stack-create-complete --stack-name $stackName
 aws cloudformation describe-stacks
-echo "stack create successfully"
+b=$(aws cloudformation describe-stacks | grep -o '"StackName": *"[^"]*"' | grep -o '"[^"]*"$' | sed 's/\"//g' | head -n 1)
+if [ $b ] && [ "$b"=="$stackName" ]; 
+then 
+echo "Stack "${stackName}" was successfully created"; 
+else 
+echo "Creation failure"; 
+fi
