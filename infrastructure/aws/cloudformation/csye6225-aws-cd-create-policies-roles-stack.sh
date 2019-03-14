1fd66c0ce5e7cd4471c6022a936eeb7a6bcfad80
junyi <fang.ju@husky.neu.edu>
2019-03-14 16:04:10 -0400

    update create policy shell

--- infrastructure/aws/cloudformation/csye6225-aws-cd-create-policies-roles-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cd-create-policies-roles-stack.sh
@@ -2,7 +2,7 @@
 set -e
 read -p "Input name of the stack: " stackName
 read -p "Input name of the bucket: " bucketName
-aws cloudformation create-stack --stack-name $stackName --template-body file://csye6225-cf-policies_roles-application.yaml --capabilities "CAPABILITY_NAMED_IAM" --parameters  "ParameterKey=bucketName,ParameterValue=code-deploy.csye6225-spring2019-$bucketName.me.csye6225.com"
+aws cloudformation create-stack --stack-name $stackName --template-body file://csye6225-cf-policies_roles-application.yaml --capabilities "CAPABILITY_NAMED_IAM" --parameters  "ParameterKey=bucketName,ParameterValue=$bucketName"
 echo "creating"
 aws cloudformation wait stack-create-complete --stack-name $stackName
 aws cloudformation describe-stacks
