82ced93d1b2a1131f80514117d64875a3955f253
leiliunu <liu.lei1@husky.neu.edu>
2019-03-16 20:48:34 -0400

    add

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-
 sudo systemctl stop tomcat.service
-
 sudo rm -rf /opt/tomcat/latest/webapps/*
-
 sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
-
 # cleanup log files
 sudo rm -rf /opt/tomcat/logs/catalina*
 sudo rm -rf /opt/tomcat/logs/*.log
