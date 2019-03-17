e5041393a2126f494d643cd80aae79dd24da79fc
leiliunu <liu.lei1@husky.neu.edu>
2019-03-16 21:22:19 -0400

    add

--- infrastructure/aws/codedeploy/beforeInstall.sh
+++ infrastructure/aws/codedeploy/beforeInstall.sh
@@ -1,4 +1,4 @@
-sudo su
-cd /opt/tomcat/latest/webapps
-rm -rf *
-echo "success"
+#!/bin/bash 
+# change to tomcat webapps directory. 
+# this directory will be different for different tomcat versions. 
+sudo rm -rf /opt/tomcat/latest/webapps/*
