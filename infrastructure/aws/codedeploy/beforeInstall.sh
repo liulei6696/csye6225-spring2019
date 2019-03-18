fa746335cdcfab6be5c49ed7c0567731e1fd20f1
leiliunu <liu.lei1@husky.neu.edu>
2019-03-18 15:39:08 -0400

    update beforeInstall and afterInstall

--- infrastructure/aws/codedeploy/beforeInstall.sh
+++ infrastructure/aws/codedeploy/beforeInstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 # change to tomcat webapps directory. 
 # this directory will be different for different tomcat versions. 
-sudo rm -rf /opt/tomcat/latest/webapps/*
+sudo su
+rm -rf /opt/tomcat/latest/webapps/*
 touch bi
