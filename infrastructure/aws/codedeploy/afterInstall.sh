8f1e83b0423fae8516ff875cdeef2a54d80ade51
junyi <fang.ju@husky.neu.edu>
2019-03-20 22:11:42 -0400

    update metrics

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -2,10 +2,15 @@
 # sudo su
 # sudo systemctl stop tomcat.service
 # sudo rm -rf /opt/tomcat/latest/webapps/*
-sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
+sudo chown tomcat:tomcat /opt/tomcat/latest/webapps/ROOT.war
 # cleanup log files
-sudo rm -rf /opt/tomcat/logs/catalina*
-sudo rm -rf /opt/tomcat/logs/*.log
-sudo rm -rf /opt/tomcat/logs/*.txt
+sudo rm -rf /opt/tomcat/latest/logs/catalina*
+sudo rm -rf /opt/tomcat/latest/logs/*.log
+sudo rm -rf /opt/tomcat/latest/logs/*.txt
 sudo systemctl restart tomcat
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/opt/tomcat/latest/webapps/cloudwatch-config.json \
+    -s
 # sudo touch ai
