5fafdbe8c5741c84782eeb6149b695fa4ba4893d
zhuziyan96 <zhu.ziy@husky.neu.edu>
2019-03-24 21:37:50 -0400

    update roles, policies and shell

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -10,7 +10,8 @@ read -p "Input name of the new stack: " stackName
 read -p "Input name of the stack you want to refer to: " refStackName
 read -p "Input name of the role stack you want to refer to: " roleStackName
 read -p "Input name of the S3 bucket you want to use: " S3BucketName
-aws cloudformation create-stack --stack-name $stackName --template-body file://csye6225-cf-application.yaml --capabilities CAPABILITY_IAM --parameters "ParameterKey=refStackName,ParameterValue=$refStackName" "ParameterKey=amiId,ParameterValue=$ami" "ParameterKey=roleStackName,ParameterValue=$roleStackName" "ParameterKey=S3BucketName,ParameterValue=$S3BucketName"
+export TOPIC_NAME=password_reset
+aws cloudformation create-stack --stack-name $stackName --template-body file://csye6225-cf-application.yaml --capabilities CAPABILITY_IAM --parameters "ParameterKey=refStackName,ParameterValue=$refStackName" "ParameterKey=amiId,ParameterValue=$ami" "ParameterKey=roleStackName,ParameterValue=$roleStackName" "ParameterKey=S3BucketName,ParameterValue=$S3BucketName" "ParameterKey=TopicName,ParameterValue=$TOPIC_NAME"
 echo "creating"
 aws cloudformation wait stack-create-complete --stack-name $stackName
 b=$(aws cloudformation describe-stacks | grep -o '"StackName": *"[^"]*"' | grep -o '"[^"]*"$' | sed 's/\"//g' | head -n 1)
