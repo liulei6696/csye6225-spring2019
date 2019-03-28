f59178704846939ff450e4ab23cfa8ba428a3a7f
leiliunu <liu.lei1@husky.neu.edu>
2019-03-28 01:56:57 -0400

    add SNS permission to EC2 and update scripts and yaml files a little

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-policies-roles-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-policies-roles-stack.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 set -e
-read -p "Input name of the stack: " stackName
-read -p "Input name of the bucket: " bucketName
+
+# cd to script directory
+work_dir=$(cd `dirname $0`; pwd)
+cd ${work_dir}
+
+read -p "Input name of this stack: " stackName
+read -p "Input name of the bucket for CodeDeploy files: " bucketName
 aws cloudformation create-stack --stack-name $stackName --template-body file://csye6225-cf-policies_roles-application.yaml --capabilities "CAPABILITY_NAMED_IAM" --parameters  "ParameterKey=bucketName,ParameterValue=$bucketName"
 echo "creating"
 aws cloudformation wait stack-create-complete --stack-name $stackName
