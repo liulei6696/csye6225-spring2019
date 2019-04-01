684260365bd939aa7ae4cf96c608c8ec7b7d47df
zhuziyan96 <zhu.ziy@husky.neu.edu>
2019-04-01 11:45:44 -0400

    modify mistakes, SSL creation failure exists

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-auto-scaling-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-auto-scaling-application-stack.sh
@@ -21,9 +21,18 @@ VpcId=`aws ec2 describe-vpcs --filter "Name=tag:Name,Values=${refStackName}-csye
 echo "Vpc found: "$VpcId
 ParamVpcId=$VpcId
 z_id=$(aws route53 list-hosted-zones --query 'HostedZones[0].Id' --output text)
-export CERTIFICATE_ARN=$(aws acm list-certificates --query "CertificateSummaryList[0].CertificateArn" --output text)
+TagKey=domainName
+TagValue=csye6225
+echo "DBServerSecurityGroupID found: "$DBServerSecurityGroupID
+echo "WebServerSecurityGroupID found: "$WebServerSecurityGroupID
+echo "LoadBalancerSecurityGroupID found: "$LoadBalancerSecurityGroupID
+echo "hostedzone id found: "$z_id
+echo "domain found: "$domainName
+# export CERTIFICATE_ARN=$(aws acm list-certificates --query "CertificateSummaryList[0].CertificateArn" --output text)
+# echo "certificate arn:"$CERTIFICATE_ARN
+# echo "Name:"$NAME
 # export TOPIC_NAME=password_reset
-aws cloudformation create-stack --stack-name $stackName --template-body file://csye6225-cf-application.yaml --capabilities CAPABILITY_IAM --parameters "ParameterKey=refStackName,ParameterValue=$refStackName" "ParameterKey=amiId,ParameterValue=$ami" "ParameterKey=roleStackName,ParameterValue=$roleStackName" "ParameterKey=S3BucketName,ParameterValue=$S3BucketName" "ParameterKey=DomainName,ParameterValue=$domainName" "ParameterKey=WebServerSecurityGroupID,ParameterValue=$WebServerSecurityGroupID" "ParameterKey=DBServerSecurityGroupID,ParameterValue=$DBServerSecurityGroupID" "ParameterKey=ParamVpcId,ParameterValue=$VpcId" "ParameterKey=CertificateArn1,ParameterValue=$CERTIFICATE_ARN" "ParameterKey=LoadBalancerName,ParameterValue=$LoadBalancerName" "ParameterKey=LoadBalancerSecurityGroupID,ParameterValue=$LoadBalancerSecurityGroupID" "ParameterKey=HostedZoneId,ParameterValue=$z_id" "ParameterKey=Name,ParameterValue=$NAME"
+aws cloudformation create-stack --stack-name $stackName --template-body file://csye6225-cf-auto-scaling-application.json --capabilities CAPABILITY_IAM --parameters "ParameterKey=refStackName,ParameterValue=$refStackName" "ParameterKey=amiId,ParameterValue=$ami" "ParameterKey=roleStackName,ParameterValue=$roleStackName" "ParameterKey=S3BucketName,ParameterValue=$S3BucketName" "ParameterKey=DomainName,ParameterValue=$domainName" "ParameterKey=WebServerSecurityGroupID,ParameterValue=$WebServerSecurityGroupID" "ParameterKey=DBServerSecurityGroupID,ParameterValue=$DBServerSecurityGroupID" "ParameterKey=ParamVpcId,ParameterValue=$VpcId" "ParameterKey=LoadBalancerName,ParameterValue=$LoadBalancerName" "ParameterKey=LoadBalancerSecurityGroupID,ParameterValue=$LoadBalancerSecurityGroupID" "ParameterKey=HostedZoneId,ParameterValue=$z_id" "ParameterKey=TagKey,ParameterValue=$TagKey" "ParameterKey=TagValue,ParameterValue=$TagValue"
 echo "creating"
 aws cloudformation wait stack-create-complete --stack-name $stackName
 b=$(aws cloudformation describe-stacks | grep -o '"StackName": *"[^"]*"' | grep -o '"[^"]*"$' | sed 's/\"//g' | head -n 1)
