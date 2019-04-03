447b366eaa8a9c305bca91bdcc5bab50cc7a4b40
junyi <fang.ju@husky.neu.edu>
2019-04-03 16:12:39 -0400

    add waf to auto scaling and change tags

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-auto-scaling-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-auto-scaling-application-stack.sh
@@ -23,16 +23,18 @@ ParamVpcId=$VpcId
 z_id=$(aws route53 list-hosted-zones --query 'HostedZones[0].Id' --output text)
 TagKey=domainName
 TagValue=csye6225
+CertificateArn1=`aws acm list-certificates --query "CertificateSummaryList[0].CertificateArn" | sed 's/\"//g'`
 echo "DBServerSecurityGroupID found: "$DBServerSecurityGroupID
 echo "WebServerSecurityGroupID found: "$WebServerSecurityGroupID
 echo "LoadBalancerSecurityGroupID found: "$LoadBalancerSecurityGroupID
 echo "hostedzone id found: "$z_id
 echo "domain found: "$domainName
+echo "certificate found: "$CertificateArn1
 # export CERTIFICATE_ARN=$(aws acm list-certificates --query "CertificateSummaryList[0].CertificateArn" --output text)
 # echo "certificate arn:"$CERTIFICATE_ARN
 # echo "Name:"$NAME
 # export TOPIC_NAME=password_reset
-aws cloudformation create-stack --stack-name $stackName --template-body file://csye6225-cf-auto-scaling-application.json --capabilities CAPABILITY_IAM --parameters "ParameterKey=refStackName,ParameterValue=$refStackName" "ParameterKey=amiId,ParameterValue=$ami" "ParameterKey=roleStackName,ParameterValue=$roleStackName" "ParameterKey=S3BucketName,ParameterValue=$S3BucketName" "ParameterKey=DomainName,ParameterValue=$domainName" "ParameterKey=WebServerSecurityGroupID,ParameterValue=$WebServerSecurityGroupID" "ParameterKey=DBServerSecurityGroupID,ParameterValue=$DBServerSecurityGroupID" "ParameterKey=ParamVpcId,ParameterValue=$VpcId" "ParameterKey=LoadBalancerName,ParameterValue=$LoadBalancerName" "ParameterKey=LoadBalancerSecurityGroupID,ParameterValue=$LoadBalancerSecurityGroupID" "ParameterKey=HostedZoneId,ParameterValue=$z_id" "ParameterKey=TagKey,ParameterValue=$TagKey" "ParameterKey=TagValue,ParameterValue=$TagValue"
+aws cloudformation create-stack --stack-name $stackName --template-body file://csye6225-cf-auto-scaling-application.json --capabilities CAPABILITY_IAM --parameters ParameterKey=CertificateArn1,ParameterValue=$CertificateArn1 ParameterKey=stackName,ParameterValue=$stackName ParameterKey=refStackName,ParameterValue=$refStackName ParameterKey=amiId,ParameterValue=$ami ParameterKey=roleStackName,ParameterValue=$roleStackName ParameterKey=S3BucketName,ParameterValue=$S3BucketName ParameterKey=DomainName,ParameterValue=$domainName ParameterKey=WebServerSecurityGroupID,ParameterValue=$WebServerSecurityGroupID ParameterKey=DBServerSecurityGroupID,ParameterValue=$DBServerSecurityGroupID ParameterKey=ParamVpcId,ParameterValue=$VpcId ParameterKey=LoadBalancerName,ParameterValue=$LoadBalancerName ParameterKey=LoadBalancerSecurityGroupID,ParameterValue=$LoadBalancerSecurityGroupID ParameterKey=HostedZoneId,ParameterValue=$z_id ParameterKey=TagKey,ParameterValue=$TagKey ParameterKey=TagValue,ParameterValue=$TagValue
 echo "creating"
 aws cloudformation wait stack-create-complete --stack-name $stackName
 b=$(aws cloudformation describe-stacks | grep -o '"StackName": *"[^"]*"' | grep -o '"[^"]*"$' | sed 's/\"//g' | head -n 1)
